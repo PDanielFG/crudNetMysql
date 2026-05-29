ac47b644877138c5efc77b95db9b822ca46a0f25
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2026-05-29 13:48:39 +0200

    Registrar el servicio de conexión en program.cs. Usando ApplicationDbContext y la cadena de conexión de appsettings.json, es lo que realiza la conexión con todo lo que hemos ido haciendo

--- crudMysqlNet/Program.cs
+++ crudMysqlNet/Program.cs
@@ -1,10 +1,21 @@
+using ControlConjuntoServer.Server.Data;
 using crudMysqlNet.Components;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+
+//Registrar el servicio con todo lo que hemos ido haciendo.
+//Voy a utilizar la clase ApplicationDbContext y la cadena de conexión, para conectarme a la bd, es lo que hace la conexión con todas las credenciales
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 
 var app = builder.Build();
 
