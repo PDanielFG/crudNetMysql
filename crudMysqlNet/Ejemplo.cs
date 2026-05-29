0a6407b0104c06997c305a7daf3167195a4aa865
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2026-05-29 13:25:46 +0200

    Creamos el proyecto con Blazor, indicando Interactivity type: Server y Interactive type: GENERAL. Creamos la clase Ejemplo, que representa una tabla en nuestra bd

---
+++ crudMysqlNet/Ejemplo.cs
@@ -0,0 +1,12 @@
+﻿namespace crudMysqlNet
+{
+
+    //Clase que representa la tabla de nuestra bd
+    public class Ejemplo
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+        public double Valor { get; set; }
+    }
+
+}
