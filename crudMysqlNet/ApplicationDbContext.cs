4de055fdb7cf97b1fdfd9d6656840e3e45280297
daniel <pedrodaniel.fernandezguerrero@alum.uca.es>
2026-05-29 13:44:51 +0200

    Creacion de la clase ApplicationDbContext, puente entre la tabla de la bd y la clase que creamos. Mapea con clase ejemplo para saber la estructura y atributos que tiene

---
+++ crudMysqlNet/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using crudMysqlNet;
+using Microsoft.EntityFrameworkCore;
+
+
+//Puente entre la tabla que creamos en la bd y la clase que creamos 
+//Estas son las tablas que existen en mi modelo
+//Mapea con la clase Ejemplo, que tiene los atributos y la estructura de la tabla
+namespace ControlConjuntoServer.Server.Data
+{
+    public class ApplicationDbContext : DbContext       // :DbContext, indica que es una clase DbContext
+    {
+        //Constructor 
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        //Referencia la tabla de la bd
+        public DbSet<Ejemplo> Ejemplos { get; set; }
+
+
+
+    }
+}
